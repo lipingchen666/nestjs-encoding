620b16b9206782fb9c83cf3c67575947c34fc07e
Liping Chen <lipingchen@Lipings-MacBook-Pro.local>
2024-04-23 13:44:33 -0400

    add time stamp

--- src/encoding/schemas/encoding.schema.ts
+++ src/encoding/schemas/encoding.schema.ts
@@ -2,8 +2,10 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
 
 export type EncodingDocument = HydratedDocument<Encoding>;
+
 export const ENCODING_SCHEMA_NAME = 'Encoding';
-@Schema()
+
+@Schema({ timestamps: true })
 export class Encoding {
   @Prop()
   description: string;
@@ -19,6 +21,12 @@ export class Encoding {
 
   @Prop({ required: true })
   userId: string;
+
+  @Prop()
+  createdAt?: Date;
+
+  @Prop()
+  updatedAt?: Date;
 }
 
 export const EncodingSchema = SchemaFactory.createForClass(Encoding);
